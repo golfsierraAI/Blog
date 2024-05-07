ba2577154bb76ef8a1daca75eaab87cfea5e709d
GolfSierra <gsharma774962@gmail.com>
2024-05-08 00:03:37 +0530

    fixing netlify image transformations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
   adapter: netlify(),
 });
